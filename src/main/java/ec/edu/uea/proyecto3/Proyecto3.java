326176221d5309a1d55c37029ded6153c8368a2b
Jose-JimenezA <joseajimenezb1@gmail.com>
2025-09-30 12:10:47 -0500

    Creacion de la clase estudiante y curso

---
+++ src/main/java/ec/edu/uea/proyecto3/Proyecto3.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.uea.proyecto3;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Proyecto3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
